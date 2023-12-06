3af751f425c678f4ed275290a5e326d92ff569d8
fernanduandrade <onanduandrade@gmail.com>
2023-12-06 10:32:54 -0300

    feat: register page

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -1,10 +1,23 @@
 <script lang="ts" setup>
-import VInput from '~/components/common/VInput.vue';
 
 const form = reactive({
   email: '',
   password: '' 
 })
+
+const loginUser = async () => {
+  const  teste = await $fetch('https://localhost:7259/api/users/login', {
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body:  JSON.stringify(form)
+  })
+}
+
+const goToRegister = () => {
+  navigateTo('/register')
+}
 const value = ref('')
 </script>
 
@@ -14,11 +27,19 @@ const value = ref('')
       <h1 class="text-5xl font-normal font-ephesis">DIARY</h1>
 
       <hr class="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700 w-full">
-
-      <VInput label="Email" v-model="form.email" type="email" placeholder="John" />
-      <VInput label="Password" v-model="form.password" type="password" placeholder="Password" />
-      <button type="button" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">LOGIN</button>
-      <button type="button" class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">SING UP</button>
+      <form @submit.prevent="loginUser" class="w-full flex flex-col gap-2">
+        <CommonVInput label="Email" v-model="form.email" type="email" placeholder="John" />
+        <CommonVInput label="Password" v-model="form.password" type="password" placeholder="Password" />
+        <button type="submit" class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">LOGIN</button>
+        <button
+          @click="goToRegister"
+          type="button"
+          class="text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
+            <NuxtLink to="/register">SIGN UP</NuxtLink>
+        </button>
+      </form>
+      
+    
     </div>
   </main>
 </template>
---
+++ pages/register/index.vue
@@ -0,0 +1,6 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <h1>oi voce vai se registrar aqui</h1>
+</template>
