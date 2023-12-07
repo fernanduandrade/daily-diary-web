4b38364aa337c3f5aac90e72d7d183b49059dcaf
fernanduandrade <onanduandrade@gmail.com>
2023-12-07 15:16:53 -0300

    fix: login form

--- pages/login/index.vue
+++ pages/login/index.vue
@@ -14,19 +14,17 @@ const { $toast } = useNuxtApp()
 
 const form = reactive({
   email: '',
-  password: '' 
+  password: ''
 })
 
 const rules = computed(() => {
   return {
     email: {
       required: helpers.withMessage('Email is required', required),
-      email: helpers.withMessage('Invalid email format',email)
+      email: helpers.withMessage('Invalid email format', email)
     },
     password: {
-      password: {
       required: helpers.withMessage('Password is required', required)
-    }
     }
   }
 })
@@ -39,14 +37,14 @@ const loginUser = async () => {
     v$.value.$errors.forEach(error => $toast.warning(error.$message.toString()))
     return
   }
-  const  { data, error } = await useFetch<ApiResponse<User>>('http://localhost:5204/api/users/login', {
+  const { data, error } = await useFetch<ApiResponse<User>>('http://localhost:5204/api/users/login', {
     headers: {
       'Content-Type': 'application/json'
     },
     method: 'POST',
-    body:  form
+    body: form
   })
-  if(error.value?.statusCode === 400){
+  if (error.value?.statusCode === 400) {
     $toast.warning(error.value.data.description)
     return
   }
@@ -62,22 +60,23 @@ const goToDash = () => navigateTo('/')
   <main class="flex items-center justify-center h-screen bg-cyan-50">
     <div class="flex p-4 justify-center shadow-md items-center flex-col gap-2 w-[35%] bg-white rounded">
       <h1 class="text-5xl mb-3 font-normal font-ephesis">Diary</h1>
-      
+
       <form @submit.prevent="loginUser" class="w-full flex flex-col gap-2">
         <CommonVInput @change="v$.email.$touch" :float-label="true" label="Email" v-model="form.email" type="email" />
-        <CommonVInput @change="v$.password.$touch" :float-label="true" label="Password" v-model="form.password" type="password" />
+        <CommonVInput @change="v$.password.$touch" :float-label="true" label="Password" v-model="form.password"
+          type="password" />
 
         <CommonVButton type="submit">Login</CommonVButton>
         <span class="text-md text-blue-400 hover:cursor-pointer hover:text-blue-600 self-end">Forgot password?</span>
-        
+
         <CommonDivider />
 
         <CommonVButton :primary="false">
           <NuxtLink to="/register">Sign up</NuxtLink>
         </CommonVButton>
       </form>
-      
-    
+
+
     </div>
   </main>
 </template>
