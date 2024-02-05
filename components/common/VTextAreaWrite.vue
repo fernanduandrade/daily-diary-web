85ff281d7ad674fdafffdd27c71ebd594df84634
fernanduandrade <onanduandrade@gmail.com>
2024-02-05 06:50:00 -0300

    feat: add line background for textarea

--- components/common/VTextAreaWrite.vue
+++ components/common/VTextAreaWrite.vue
@@ -17,6 +17,15 @@ defineProps({
   <label for="message" class="block text-md font-bold text-gray-900 dark:text-white">
     {{ label }}
   </label>
-  <textarea :value="modelValue" @input="$emit('update:modelValue', ($event.target as HTMLTextAreaElement).value)" id="message" rows="4" class="h-full font-ephesis text-3xl block p-2.5 w-full font-semibold tracking-tight text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+  <textarea :value="modelValue" @input="$emit('update:modelValue', ($event.target as HTMLTextAreaElement).value)" id="message" class="h-full font-ephesis text-3xl block p-1 w-full font-semibold tracking-tight text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 </main>
 </template>
+
+<style>
+textarea {
+  background-image: linear-gradient(white 40%, rgb(241, 241, 241) 40%);
+  background-size: 100% 5rem;
+  width: 100%;
+  padding: 4px 8px;
+}
+</style>
